21a805c23067fb43a752f4cfad5212e981503fdf
alaya yasmine <yesminealaya12.gmail.com>
2023-12-15 16:15:14 +0100

    Task1

--- number_game.java
+++ number_game.java
@@ -9,25 +9,58 @@ public class number_game{
         int randomnbr=random.nextInt(100);
         System.out.println("the random number is:"+randomnbr);
         boolean test=false;
+        
+        int round=0;
+        int score=100;
         do{
-            System.out.println("guess the number");
-            Scanner scanner=new Scanner(System.in);
-            int nbguess=scanner.nextInt();
-            System.out.println(nbguess);
-            if(nbguess==randomnbr){
-                System.out.println("your guess is correct");
-                test=true;
-            }
-            else{
-                if(nbguess>randomnbr){
-                    System.out.println("your guess is too high");
-                    test=false;
+            int i=0;
+            System.out.println("round number:"+round);
+            do{
+                System.out.println("guess the number");
+                Scanner scanner=new Scanner(System.in);
+                int nbguess=scanner.nextInt();
+                
+                if(nbguess==randomnbr){
+                    System.out.println("your guess is correct");
+                    test=true;
+                    break;
                 }
                 else{
-                    System.out.println("your guess is too low");
-                    test=false;
+                    if(nbguess>randomnbr){
+                        System.out.println("your guess is too high");
+                        test=false;
+                        i++;
+                    }
+                    else{
+                        System.out.println("your guess is too low");
+                        test=false;
+                        i++;
+                    }
                 }
-            }
-        } while(test==false);
+            } while(i<4);
+            round++;
+            
+        }while (round<4 && test==false);
+        if(round==4 && test==false){
+            System.out.println("vous avez perdu");
+        }
+        System.out.println("round is"+round);
+        switch(round){
+            case 1:
+            System.out.println("ton score est:"+score);
+            break;
+            case 2:
+            score=score-20;
+            System.out.println("ton score est:"+score);
+            break;
+            case 3:
+            score=score-40;
+            System.out.println("ton score est:"+score);
+            break;    
+        
+        }
+    
+        
     }
+    
 }
