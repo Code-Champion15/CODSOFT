d2774a7e4004b22a8cdf6d42e1564e1c650c561d
alaya yasmine <yesminealaya12.gmail.com>
2023-12-15 14:16:55 +0100

    tasks 3&4

--- number_game.java
+++ number_game.java
@@ -8,22 +8,26 @@ public class number_game{
         Random random=new Random();
         int randomnbr=random.nextInt(100);
         System.out.println("the random number is:"+randomnbr);
-        System.out.println("guess the number");
-        Scanner scanner=new Scanner(System.in);
-        int nbguess=scanner.nextInt();
-        System.out.println(nbguess);
-        if(nbguess==randomnbr){
-            System.out.println("your guess is correct");
-        }
-        else{
-            if(nbguess>randomnbr){
-                System.out.println("your guess is too high");
+        boolean test=false;
+        do{
+            System.out.println("guess the number");
+            Scanner scanner=new Scanner(System.in);
+            int nbguess=scanner.nextInt();
+            System.out.println(nbguess);
+            if(nbguess==randomnbr){
+                System.out.println("your guess is correct");
+                test=true;
             }
             else{
-                System.out.println("your guess is too low");
+                if(nbguess>randomnbr){
+                    System.out.println("your guess is too high");
+                    test=false;
+                }
+                else{
+                    System.out.println("your guess is too low");
+                    test=false;
+                }
             }
-        }
-        
-
+        } while(test==false);
     }
 }
