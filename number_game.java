19a2caddc2a3fc24a69b263b426e8ee3ce95cec6
alaya yasmine <yesminealaya12.gmail.com>
2023-12-15 11:14:56 +0100

    task1

---
+++ number_game.java
@@ -0,0 +1,9 @@
+public class number_game{
+    public static void main(string args[]){
+        int min=1;
+        int max=100;
+        Random random=new Random();
+        int randomnbr=random.nextInt(100);
+        System.out.println("the random number is:"+randomnbr);
+    }
+}
