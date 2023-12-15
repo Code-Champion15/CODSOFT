97b66a225477c2fddbf8801ff09deb851290da3a
alaya yasmine <yesminealaya12.gmail.com>
2023-12-15 12:29:41 +0100

    task2v1

--- number_game.java
+++ number_game.java
@@ -1,9 +1,15 @@
+
+import java.util.Random;
+import java.util.Scanner;
 public class number_game{
-    public static void main(string args[]){
+    public static void main(String args[]){
         int min=1;
         int max=100;
         Random random=new Random();
         int randomnbr=random.nextInt(100);
         System.out.println("the random number is:"+randomnbr);
+        System.out.println("guess the number");
+        Scanner nbguess=new Scanner(System.in);
+        System.out.println(nbguess);
     }
 }
