cdd52476d7806fd09f61ed093207f7916de95ccd
alaya yasmine <yesminealaya12.gmail.com>
2023-12-29 21:42:10 +0100

    task 4 class quiz

--- quiz.java
+++ quiz.java
@@ -2,58 +2,97 @@ import java.util.*;
 
 public class quiz{
     ArrayList<questions> qst;
-    private int score;
-    private int indicecurrentqst ; //current questio, index
+    protected int score;
+    protected int indicecurrentqst ; //current questio index
+    protected int userrep;//the answer of the user
 
-    public void setScore(int score){
+    /*public void setScore(int score){
         this.score=score;
     }
-    
+
     public int getScore(){
         return score;
+    }*/
+
+     public void setUserrep(int userrep){
+        this.userrep=userrep;
     }
 
-    public void setIndicecurrentqst(int indicecurrentqst){
+    public int getUserrep(){
+        return userrep;
+    }
+
+    /*public void setIndicecurrentqst(int indicecurrentqst){
         this.indicecurrentqst=indicecurrentqst;
     }
     
-    public String getIndicecurrentqst(){
+    public int getIndicecurrentqst(){
         return indicecurrentqst;
-    }
+    }*/
 
     public quiz(ArrayList<questions> qst ){
-        this.qst=qst;
+        this.qst=qst; //qst is the name of the array list which contain questions of the quiz
         this.score=0;
         this.indicecurrentqst=0;
     }
-    void displayquestion(){
-        questions questions = qst.get(indicecurrentqst);
-        System.out.println(question.question);
-        for(int i=0;i<questions.choix.size();i++){
-            System.out.println((i+1)+"."+questions.choix.get(i));
+    public void displayquestion(){
+        questions currentqst = qst.get(indicecurrentqst);
+        //affiche currentqst
+        System.out.println("Question " + (indicecurrentqst + 1) + ": " + currentqst.getQst());
+        //System.out.println(currentqst.questions);
+        for(int i=0;i<currentqst.choix.size();i++){
+            System.out.println((i+1)+"."+currentqst.choix.get(i));
 
         }
     }
-    void submitAnswer(int userrep){
-        questions questions = qst.get(indicecurrentqst);
-        if (userrep==questions.repcorrecte){
+    public void submitAnswer(int userrep){
+        questions currentqst = qst.get(indicecurrentqst);
+        if (userrep==currentqst.getRepcorrecte()){
             score++;
         }
     }
-    void displayresult(){
-        System.out.println("terminee");
+    public void displayresult(){
+        System.out.println("quiz terminé");
         System.out.println("ton score final est:"+score);
         System.out.println("recap:");
         for(int i = 0; i<qst.size();i++){
             questions questions = qst.get(i);
             String status;
-             if(questions.repcorrecte==questions.userrep){
+             if(questions.getRepcorrecte()==userrep){
                 status="correcte";
 
              }else{
                 status="incorrecte";
              }
-             System.out.println((i+1)+"."+status);
+             System.out.println((i+1)+"."+status);  
         }
     }
+    public void runquizz(){
+        Scanner e=new Scanner(System.in);
+        for(questions currentqst : qst){
+            displayquestion();
+            long start= System.currentTimeMillis();
+            int timerSec = 10;
+            while (System.currentTimeMillis()-start<timerSec*1000){
+                System.out.println("saisir votre reponse 1-4:");
+                if(e.hasNextLine()){
+                    int userrep = e.nextInt();
+                    if(userrep>=1 && userrep<=4){
+                        setUserrep(userrep);
+                        break;
+                    }else{
+                        System.out.println("reponse invalide veillez entrer un nombre entre 1 et 4");
+                    }
+                }else{
+                    System.out.println("saisir une autre fois un nombre valide entre 1 et 4");
+                    e.nextInt();
+                }
+            }
+            submitAnswer(getUserrep());
+            indicecurrentqst=indicecurrentqst+1;
+        }
+        displayresult();
+    
+    }
+
 }
